2b0818cc21ffb481e4df34b9132134abc13652cf
pandey503 <ashish.pandey@healthandglow.com>
2021-07-23 20:09:53 +0530

    void

--- hng-git-poc/src/main/java/com/demo/git/HngPostgresPoc1Application.java
+++ hng-git-poc/src/main/java/com/demo/git/HngPostgresPoc1Application.java
@@ -9,9 +9,5 @@ public class HngPostgresPoc1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(HngPostgresPoc1Application.class, args);
 	}
-	// D1
-	// D2
-	// D3
-	// D5
-	// D6
+	
 }
