c3133989585b524d848d7d4e021c689e485b1aa6
pandey503 <ashish.pandey@healthandglow.com>
2021-07-23 19:55:00 +0530

    changes done

--- hng-git-poc/src/main/java/com/demo/git/HngPostgresPoc1Application.java
+++ hng-git-poc/src/main/java/com/demo/git/HngPostgresPoc1Application.java
@@ -11,36 +11,9 @@ public class HngPostgresPoc1Application {
 	}
 	//D1
 	//D2
-<<<<<<< Upstream, based on origin/develop
-<<<<<<< Upstream, based on origin/develop
-<<<<<<< Upstream, based on origin/develop
-<<<<<<< Upstream, based on origin/develop
 	//D3
-<<<<<<< Upstream, based on origin/develop
 	//D4
-=======
-=======
->>>>>>> fdd2b2b D3
-<<<<<<< Upstream, based on origin/develop
-=======
-=======
-=======
->>>>>>> 476d103 equlize
 	//D3
-<<<<<<< Upstream, based on origin/develop
->>>>>>> f61fa2f D3
-	
-	
->>>>>>> 6f6c0b2 more remove
->>>>>>> 88abe6d more remove
-=======
-=======
-	
->>>>>>> 5ad82df equal
-
->>>>>>> 476d103 equlize
-=======
 	//D5
 	//D6
->>>>>>> da048a3 D5D6
 }
